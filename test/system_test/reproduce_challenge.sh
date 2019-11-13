596a927c20a5a229b53d23020b59ecb5a649f759
Mattia Tomasoni <tomasonimattia@gmail.com>
2019-11-13 15:57:42 +0100

    removing some old comments

--- test/system_test/reproduce_challenge.sh
+++ test/system_test/reproduce_challenge.sh
@@ -39,32 +39,32 @@ rm -f $HOME/.monet/containers/*/singularity/*.img
 
 # R1 - CAUSALITY
 monet --input=./input/1_ppi_anonym_v2.txt --output=$output --method=R1 --container=singularity \
-  --b=2.4 --c=800 --i=2 --filter=double --threshold=4 --post=recluster --smallest=3 --largest=100 --b2=2.4 --c2=800 --i2=2 #ERROR ERROR  : No valid /bin/sh in container
+  --b=2.4 --c=800 --i=2 --filter=double --threshold=4 --post=recluster --smallest=3 --largest=100 --b2=2.4 --c2=800 --i2=2
 monet --input=./input/1_ppi_anonym_v2.txt --output=$output --method=R1 --container=docker \
   --b=2.4 --c=800 --i=2 --filter=double --threshold=4 --post=recluster --smallest=3 --largest=100 --b2=2.4 --c2=800 --i2=2
 
 monet --input=./input/2_ppi_anonym_v2.txt --output=$output --method=R1 --container=singularity \
-  --b=1.3 --c=500 --i=2 --filter=pageRank --threshold=3 --post=recluster --smallest=5 --largest=100 --b2=1.3 --c2=500 --i2=2 #ERROR
+  --b=1.3 --c=500 --i=2 --filter=pageRank --threshold=3 --post=recluster --smallest=5 --largest=100 --b2=1.3 --c2=500 --i2=2 
 monet --input=./input/2_ppi_anonym_v2.txt --output=$output --method=R1 --container=docker \
   --b=1.3 --c=500 --i=2 --filter=pageRank --threshold=3 --post=recluster --smallest=5 --largest=100 --b2=1.3 --c2=500 --i2=2
 
 monet --input=./input/3_signal_anonym_directed_v3.txt --output=$output --method=R1 --container=singularity \
-  --b=1.7 --c=400 --i=2 --filter=quantile --threshold=1 --post=discard --smallest=3 --largest=100 --b2=1.7 --c2=500 --i2=2 #ERROR
+  --b=1.7 --c=400 --i=2 --filter=quantile --threshold=1 --post=discard --smallest=3 --largest=100 --b2=1.7 --c2=500 --i2=2 
 monet --input=./input/3_signal_anonym_directed_v3.txt --output=$output --method=R1 --container=docker \
   --b=1.7 --c=400 --i=2 --filter=quantile --threshold=1 --post=discard --smallest=3 --largest=100 --b2=1.7 --c2=500 --i2=2
 
 monet --input=./input/4_coexpr_anonym_v2.txt --output=$output --method=R1 --container=singularity \
-  --b=1.8 --c=600 --i=2 --filter=quantile --threshold=4 --post=recluster --smallest=3 --largest=100 --b2=0.5 --c2=800 --i2=2 #ERROR
+  --b=1.8 --c=600 --i=2 --filter=quantile --threshold=4 --post=recluster --smallest=3 --largest=100 --b2=0.5 --c2=800 --i2=2 
 monet --input=./input/4_coexpr_anonym_v2.txt --output=$output --method=R1 --container=docker \
   --b=1.8 --c=600 --i=2 --filter=quantile --threshold=4 --post=recluster --smallest=3 --largest=100 --b2=0.5 --c2=800 --i2=2
 
 monet --input=./input/5_cancer_anonym_v2.txt --output=$output --method=R1 --container=singularity \
-  --b=2 --c=600 --i=1.8 --filter=quantile --threshold=2 --post=discard --smallest=5 --largest=100 --b2=2 --c2=600 --i2=1.8 #ERROR
+  --b=2 --c=600 --i=1.8 --filter=quantile --threshold=2 --post=discard --smallest=5 --largest=100 --b2=2 --c2=600 --i2=1.8 
 monet --input=./input/5_cancer_anonym_v2.txt --output=$output --method=R1 --container=docker \
   --b=2 --c=600 --i=1.8 --filter=quantile --threshold=2 --post=discard --smallest=5 --largest=100 --b2=2 --c2=600 --i2=1.8
 
 monet --input=./input/6_homology_anonym_v2.txt --output=$output --method=R1 --container=singularity \
-  --b=3.5 --c=1000 --i=2 --filter=double --threshold=4 --post=recluster --smallest=10 --largest=100 --b2=0.5 --c2=800 --i2=2 #ERROR
+  --b=3.5 --c=1000 --i=2 --filter=double --threshold=4 --post=recluster --smallest=10 --largest=100 --b2=0.5 --c2=800 --i2=2 
 monet --input=./input/6_homology_anonym_v2.txt --output=$output --method=R1 --container=docker \
   --b=3.5 --c=1000 --i=2 --filter=double --threshold=4 --post=recluster --smallest=10 --largest=100 --b2=0.5 --c2=800 --i2=2
 
@@ -98,10 +98,10 @@ monet --input=./input/2_ppi_anonym_v2.txt --output=$output --method=K1 --contain
 monet --input=./input/2_ppi_anonym_v2.txt --output=$output --method=K1 --container=docker
 
 monet --input=./input/3_signal_anonym_directed_v3.txt --output=$output --method=K1 --container=singularity
-monet --input=./input/3_signal_anonym_directed_v3.txt --output=$output --method=K1 --container=docker # seg fault while building the container
+monet --input=./input/3_signal_anonym_directed_v3.txt --output=$output --method=K1 --container=docker
 
 monet --input=./input/4_coexpr_anonym_v2.txt --output=$output --method=K1 --container=singularity#OK
-monet --input=./input/4_coexpr_anonym_v2.txt --output=$output --method=K1 --container=docker #OK
+monet --input=./input/4_coexpr_anonym_v2.txt --output=$output --method=K1 --container=docker
 
 monet --input=./input/5_cancer_anonym_v2.txt --output=$output --method=K1 --container=singularity
 monet --input=./input/5_cancer_anonym_v2.txt --output=$output --method=K1 --container=docker
